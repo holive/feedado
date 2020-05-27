719662262708f7ce49871d742df4a6f12f5762dd
Hugo Oliveira <6090596+holive@users.noreply.github.com>
2020-05-26 22:29:32 -0300

    fix (feed): check source before create

--- app/feed/service.go
+++ app/feed/service.go
@@ -2,6 +2,8 @@ package feed
 
 import (
 	"context"
+	"net/url"
+	"strings"
 
 	"github.com/pkg/errors"
 
@@ -14,17 +16,23 @@ type Service struct {
 }
 
 func (s *Service) Create(ctx context.Context, feed *Feed) (*Feed, error) {
+	if err := s.validateURL(feed.Source); err != nil {
+		return &Feed{}, err
+	}
+
+	feed.Source = strings.TrimSuffix(feed.Source, "/")
+
 	alreadyExists, _ := s.repo.FindBySource(ctx, feed.Source)
 	if alreadyExists != nil {
 		return &Feed{}, errors.New("source already exists")
 	}
 
-	feed, err := s.repo.Create(ctx, feed)
+	newFeed, err := s.repo.Create(ctx, feed)
 	if err != nil {
 		return &Feed{}, errors.Wrap(err, "could not create a feed")
 	}
 
-	return feed, nil
+	return newFeed, nil
 }
 
 func (s *Service) Update(ctx context.Context, feed *Feed) error {
@@ -41,6 +49,19 @@ func (s *Service) FindBySource(ctx context.Context, source string) (*Feed, error
 
 func (s *Service) FindAll(ctx context.Context, limit string, offset string) (*SearchResult, error) {
 	return s.repo.FindAll(ctx, limit, offset)
+}
+
+func (s *Service) validateURL(source string) error {
+	u, err := url.Parse(source)
+	if (err == nil && u.Scheme != "" && u.Host != "") == false {
+		return errors.New("invalid url")
+	}
+
+	if strings.HasPrefix(source, "https://") == false {
+		return errors.New("source must have 'https://' as prefix") // TODO: is that right?
+	}
+
+	return nil
 }
 
 func NewService(repository Repository, client infraHTTP.Runner) *Service {
