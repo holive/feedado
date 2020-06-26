e34f8fd57c414523a74d14155896b2d740ddb8f4
Hugo Oliveira <6090596+holive@users.noreply.github.com>
2020-06-26 10:10:11 -0300

    refact: logger

--- app/feedado/logger.go
+++ app/feedado/logger.go
@@ -4,23 +4,41 @@ import (
 	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func initLogger() (*zap.SugaredLogger, error) {
 	var (
-		logger  *zap.Logger
+		cfg = zap.Config{
+			Level:             zap.NewAtomicLevelAt(zapcore.DebugLevel),
+			Development:       false,
+			DisableStacktrace: true,
+			Encoding:          "json",
+			EncoderConfig: zapcore.EncoderConfig{
+				TimeKey:        "ts",
+				LevelKey:       "level",
+				NameKey:        "logger",
+				CallerKey:      "caller",
+				MessageKey:     "msg",
+				StacktraceKey:  "stacktrace",
+				LineEnding:     zapcore.DefaultLineEnding,
+				EncodeLevel:    zapcore.LowercaseLevelEncoder,
+				EncodeTime:     zapcore.ISO8601TimeEncoder,
+				EncodeDuration: zapcore.SecondsDurationEncoder,
+				EncodeCaller:   zapcore.ShortCallerEncoder,
+			},
+			OutputPaths:      []string{"stderr"},
+			ErrorOutputPaths: []string{"stderr"},
+		}
 		err     error
 		profile = os.Getenv("APP_PROFILE")
 	)
 
 	if profile == "production" {
-		logger, err = zap.NewProduction()
-		if err != nil {
-			return nil, err
-		}
+		cfg.Level.SetLevel(zapcore.InfoLevel)
 	}
 
-	logger, err = zap.NewDevelopment()
+	logger, err := cfg.Build()
 	if err != nil {
 		return nil, err
 	}
